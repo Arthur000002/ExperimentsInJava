38f0606f8199c269d5e51451e4e1431d85f48270
Arthur000002 <artp01656@gmail.com>
2025-07-07 09:53:20 +0300

    Add files via upload

---
+++ src/main/java/com/experiments/experimentsinjavatemplate/ExperimentsInJavaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.experiments.experimentsinjavatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentsInJavaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentsInJavaTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/experiments/experimentsinjavatemplate/ExperimentsInJavaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiments.experimentsinjavatemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentsInJavaTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
